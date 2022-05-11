cfefb3a0c1090ec75d00d9e1abd0a1d380f29619
daichitakahashi <t.daichi.snd@gmail.com>
2022-05-12 00:40:02 +0900

    Update: implement network creation and start/stop container
    todo: remove network

--- container.go
+++ container.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -14,37 +15,42 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 type Group struct {
 	cli       *client.Client
 	namespace string
-	network   string
+	network   *types.NetworkResource
 }
 
-func NewGroup(tb testing.TB, network string, opts ...client.Opt) *Group {
+func NewGroup(ctx context.Context, tb testing.TB, namespace, network string, opts ...client.Opt) *Group {
 	tb.Helper()
 
-	ctx := context.Background()
-	namespace := os.Getenv("CFT_NAMESPACE")
+	if namespace == "" {
+		namespace = os.Getenv("CFT_NAMESPACE")
+	}
 	if network == "" {
 		network = os.Getenv("CFT_NETWORK")
 	} else if namespace != "" && network != "" {
-		network = namespace + "/" + network
+		network = namespace + "-" + network
 	}
 
 	cli, err := client.NewClientWithOpts(opts...)
 	if err != nil {
 		tb.Fatal(err)
 	}
+	cli.NegotiateAPIVersion(ctx)
 
+	var nw *types.NetworkResource
 	if network != "" {
 		// ネットワークの存在を確認、なければ作成
 		list, err := cli.NetworkList(ctx, types.NetworkListOptions{})
 		if err != nil {
 			tb.Fatal(err)
 		}
+
 		var found bool
 		for _, n := range list {
 			if n.Name == network {
@@ -53,93 +59,198 @@ func NewGroup(tb testing.TB, network string, opts ...client.Opt) *Group {
 			}
 		}
 		if !found {
-			_, err = cli.NetworkCreate(ctx, network, types.NetworkCreate{
+			// TODO: **remove network**
+			created, err := cli.NetworkCreate(ctx, network, types.NetworkCreate{
 				Driver: "bridge",
 			})
 			if err != nil {
 				tb.Fatal(err)
 			}
+			n, err := cli.NetworkInspect(ctx, created.ID, types.NetworkInspectOptions{
+				Verbose: true,
+			})
+			if err != nil {
+				tb.Fatal(err)
+			}
+			nw = &n
 		}
 	}
 
 	return &Group{
 		cli:       cli,
 		namespace: namespace,
-		network:   network,
+		network:   nw,
 	}
 }
 
 type Container struct {
-	container.Config
-	name    string
-	WaitFor wait.Strategy
+	Image        string
+	Env          []string
+	Cmd          []string
+	Entrypoint   []string
+	ExposedPorts []string
+	WaitFor      wait.Strategy
 }
 
-// TODO: delay run option
-
-func (l *Group) Run(tb testing.TB, name string, c *Container) map[string]string {
-	tb.Helper()
-	ctx := context.Background()
-
-	if l.namespace != "" {
-		name = l.namespace + "/" + name
-	}
-
-	hostConfig := container.HostConfig{ // TODO: option
-		Binds:           nil,
-		ContainerIDFile: "",
-		LogConfig:       container.LogConfig{},
-		NetworkMode:     "",
-		PortBindings:    nil,
-		RestartPolicy:   container.RestartPolicy{},
-		AutoRemove:      false,
-		VolumeDriver:    "",
-		VolumesFrom:     nil,
-		CapAdd:          nil,
-		CapDrop:         nil,
-		CgroupnsMode:    "",
-		DNS:             nil,
-		DNSOptions:      nil,
-		DNSSearch:       nil,
-		ExtraHosts:      nil,
-		GroupAdd:        nil,
-		IpcMode:         "",
-		Cgroup:          "",
-		Links:           nil,
-		OomScoreAdj:     0,
-		PidMode:         "",
-		Privileged:      false,
-		PublishAllPorts: false,
-		ReadonlyRootfs:  false,
-		SecurityOpt:     nil,
-		StorageOpt:      nil,
-		Tmpfs:           nil,
-		UTSMode:         "",
-		UsernsMode:      "",
-		ShmSize:         0,
-		Sysctls:         nil,
-		Runtime:         "",
-		ConsoleSize:     [2]uint{},
-		Isolation:       "",
-		Resources:       container.Resources{},
-		Mounts:          nil,
-		MaskedPaths:     nil,
-		ReadonlyPaths:   nil,
-		Init:            nil,
-	}
-	networkConfig := network.NetworkingConfig{} // TODO: option
-
-	l.cli.ContainerCreate(ctx, &c.Config, &hostConfig, &networkConfig, nil, name)
-	return nil
+type RunOption struct {
+	// Lazy          bool // TODO: Group.LazyRun(ctx, &confort.M{}, name, c)???
+	ContainerConfig func(config *container.Config)
+	HostConfig      func(config *container.HostConfig)
+	NetworkConfig   func(config *network.NetworkingConfig)
+	PullOptions     *types.ImagePullOptions
+	AutoCleanup     bool
 }
 
-func (l *Group) BuildAndRun(tb testing.TB, dockerfile string, skip bool, name string, c *Container) map[string]string {
+type TerminateFunc func()
+
+func (g *Group) Run(ctx context.Context, tb testing.TB, name string, c *Container, opt RunOption) (map[nat.Port]string, TerminateFunc) {
 	tb.Helper()
-	ctx := context.Background()
+
+	if g.namespace != "" {
+		name = g.namespace + "-" + name
+	}
+
+	// find existing container
+	existing, err := g.existingContainer(ctx, c.Image, name)
+	if err != nil {
+		tb.Fatal(err)
+	} else if existing != nil {
+		ports := map[nat.Port]string{}
+		for _, p := range existing.Ports {
+			if p.IP != "" {
+				np, err := nat.NewPort(p.Type, fmt.Sprint(p.PrivatePort))
+				if err != nil {
+					tb.Fatal(err)
+				}
+				ports[np] = p.IP + ":" + fmt.Sprint(p.PublicPort)
+			}
+		}
+		return ports, func() {}
+	}
+
+	if opt.PullOptions != nil {
+		rc, err := g.cli.ImagePull(ctx, c.Image, *opt.PullOptions)
+		if err != nil {
+			tb.Fatalf("pull: %s", err)
+		}
+		_, err = io.ReadAll(rc)
+		if err != nil {
+			tb.Fatalf("pull: %s", err)
+		}
+		err = rc.Close()
+		if err != nil {
+			tb.Fatalf("pull: %s", err)
+		}
+	}
+
+	portSet, portBindings, err := nat.ParsePortSpecs(c.ExposedPorts)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	cc := &container.Config{
+		Image:        c.Image,
+		ExposedPorts: portSet,
+		Env:          c.Env,
+		Cmd:          c.Cmd,
+		Entrypoint:   c.Entrypoint,
+	}
+	if opt.ContainerConfig != nil {
+		opt.ContainerConfig(cc)
+	}
+	hc := &container.HostConfig{
+		PortBindings: portBindings,
+		AutoRemove:   true,
+	}
+	if opt.HostConfig != nil {
+		opt.HostConfig(hc)
+	}
+	nc := &network.NetworkingConfig{}
+	if g.network != nil {
+		nc.EndpointsConfig = map[string]*network.EndpointSettings{
+			g.network.ID: {
+				NetworkID: g.network.ID,
+			},
+		}
+	}
+	if opt.NetworkConfig != nil {
+		opt.NetworkConfig(nc)
+	}
+
+	created, err := g.cli.ContainerCreate(ctx, cc, hc, nc, nil, name)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	err = g.cli.ContainerStart(ctx, created.ID, types.ContainerStartOptions{})
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	var success bool
+	terminate := func() {
+		err := g.cli.ContainerStop(context.Background(), created.ID, nil)
+		if err != nil {
+			tb.Log(err)
+		}
+	}
+	defer func() {
+		if !success {
+			terminate()
+		}
+	}()
+
+	i, err := g.cli.ContainerInspect(ctx, created.ID)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	ports := map[nat.Port]string{}
+	for p, bindings := range i.NetworkSettings.Ports {
+		if len(bindings) == 0 {
+			continue
+		}
+		b := bindings[0]
+		if b.HostPort == "" {
+			continue
+		}
+		ports[p] = b.HostIP + ":" + b.HostPort
+	}
+
+	if opt.AutoCleanup {
+		tb.Cleanup(terminate)
+		terminate = func() {}
+	}
+
+	success = true
+	return ports, terminate
+}
+
+func (g *Group) existingContainer(ctx context.Context, image, name string) (*types.Container, error) {
+	name = "/" + name
+
+	containers, err := g.cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	for _, c := range containers {
+		for _, n := range c.Names {
+			if name == n {
+				if c.Image == image {
+					return &c, nil
+				}
+				return nil, fmt.Errorf("container name %q already exists but image is not %q(%q)", name, image, c.Image)
+			}
+		}
+	}
+	return nil, nil
+}
+
+func (g *Group) BuildAndRun(ctx context.Context, tb testing.TB, dockerfile string, skip bool, name string, c *Container, opt RunOption) (map[nat.Port]string, TerminateFunc) {
+	tb.Helper()
 
 	// 指定の名前のイメージが既に存在するかどうかの確認
 	var found bool
-	summaries, err := l.cli.ImageList(ctx, types.ImageListOptions{
+	summaries, err := g.cli.ImageList(ctx, types.ImageListOptions{
 		All: true,
 	})
 	if err != nil {
@@ -176,7 +287,7 @@ LOOP:
 			tb.Fatal(err)
 		}
 
-		resp, err := l.cli.ImageBuild(ctx, archived, types.ImageBuildOptions{
+		resp, err := g.cli.ImageBuild(ctx, archived, types.ImageBuildOptions{
 			Dockerfile: dockerfileName,
 			Tags:       []string{c.Image},
 			Remove:     true,
@@ -212,7 +323,8 @@ LOOP:
 		tb.Logf("image %q already exists", c.Image)
 	}
 
-	return l.Run(tb, name, c)
+	opt.PullOptions = nil
+	return g.Run(ctx, tb, name, c, opt)
 }
 
 func archive(dockerfileName, dockerfile string) (io.Reader, error) {
