fcc5eea8024b8b13b4873730d4bf7af2a51b825a
daichitakahashi <t.daichi.snd@gmail.com>
2022-06-11 23:33:10 +0900

    Implement UniqueValueServiceServer.StoreUniqueValue

---
+++ beacon/server/unique_test.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/daichitakahashi/confort/proto/beacon"
+)
+
+func TestUniqueValueServer_StoreUniqueValue(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	cli := beacon.NewUniqueValueServiceClient(conn)
+
+	testCases := []struct {
+		store   string
+		value   string
+		success bool
+	}{
+		{"uuid", "cac66b57-3f31-4f44-915e-1efe083147f1", true},
+		{"uuid", "fc72ad70-9190-41c3-b327-65afbe751d01", true},
+		{"uuid", "fc72ad70-9190-41c3-b327-65afbe751d01", false},
+		{"uuid", "cac66b57-3f31-4f44-915e-1efe083147f1", false},
+		{"uuid", "b2119847-a173-460e-8b4e-0218e084fa58", true},
+		{"another-uuid", "cac66b57-3f31-4f44-915e-1efe083147f1", true},
+		{"another-uuid", "cac66b57-3f31-4f44-915e-1efe083147f1", false},
+		{"another-uuid", "fc72ad70-9190-41c3-b327-65afbe751d01", true},
+	}
+
+	for _, tc := range testCases {
+		resp, err := cli.StoreUniqueValue(ctx, &beacon.StoreUniqueValueRequest{
+			Store: tc.store,
+			Value: tc.value,
+		})
+		if err != nil {
+			t.Fatalf("store: %s, value: %s, err: %s", tc.store, tc.value, err)
+		}
+		want := resp.GetSucceeded()
+		got := tc.success
+		if got != want {
+			t.Errorf("store: %s, value: %s, want: %s, got: %s",
+				tc.store, tc.value, result(want), result(got))
+		}
+	}
+}
+
+func result(succeeded bool) string {
+	if succeeded {
+		return "succeeded"
+	}
+	return "failed"
+}
