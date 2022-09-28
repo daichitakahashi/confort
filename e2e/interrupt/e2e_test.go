e9419177afa5d8b634fd3814d110591d7e1cba59
daichitakahashi <t.daichi.snd@gmail.com>
2022-09-28 12:11:01 +0900

    Add e2e/interrupt: test the behavior after `confort test` has interrupted

---
+++ e2e/interrupt/e2e_test.go
@@ -0,0 +1,94 @@
+package interrupt
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/google/uuid"
+)
+
+func TestTest_Interrupted(t *testing.T) {
+	ctx := context.Background()
+
+	namespace := uuid.NewString()
+	cmd := exec.Command("go", "run", "../../cmd/confort", "test", "-go=mod", "-namespace", namespace,
+		"--", "-v", "-tags=interrupt", "./tests")
+	buf := bytes.NewBuffer(nil)
+	w := io.MultiWriter(os.Stdout, buf)
+	cmd.Stdout = w
+	cmd.Stderr = w
+
+	// Set new process group id.
+	// After the container is created, we send SIGINT to "confort test".
+	// Because the command is executed via go command(not a built binary), we
+	// have to use a new process group to send the signal.
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	err := cmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cli.NegotiateAPIVersion(ctx)
+	containerExists := func(t *testing.T) bool {
+		t.Helper()
+		name := "/" + namespace + "-container"
+		list, err := cli.ContainerList(ctx, types.ContainerListOptions{
+			Filters: filters.NewArgs(
+				filters.Arg("name", name),
+			),
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, c := range list {
+			for _, n := range c.Names {
+				if n == name {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	var found bool
+	for i := 0; i < 20; i++ {
+		if containerExists(t) {
+			found = true
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	if !found {
+		t.Fatal("container not exists")
+	}
+	t.Log("container exists")
+
+	// Send SIGINT to created process group.
+	err = syscall.Kill(-cmd.Process.Pid, syscall.SIGINT)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Wait for test command has finished.
+	err = cmd.Wait()
+	t.Log("confort test finished: ", err)
+
+	// Check whether the container is removed.
+	if containerExists(t) {
+		t.Fatal("container is not removed")
+	}
+}
