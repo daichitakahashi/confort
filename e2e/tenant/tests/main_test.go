de9c3f472f4d3be531f531e674d79faec49fd10f
daichitakahashi <t.daichi.snd@gmail.com>
2022-11-09 00:17:21 +0900

    Update: use testingc v0.0.2

--- e2e/tenant/tests/main_test.go
+++ e2e/tenant/tests/main_test.go
@@ -2,13 +2,13 @@ package tests
 
 import (
 	"context"
+	"log"
 	"os"
 	"testing"
 
 	"github.com/daichitakahashi/confort/e2e/tenant/database"
 	"github.com/daichitakahashi/confort/internal/beacon"
 	"github.com/daichitakahashi/confort/unique"
-	"github.com/daichitakahashi/testingc"
 )
 
 var (
@@ -17,28 +17,31 @@ var (
 	uniqueEmployeeUserName *unique.Unique[string]
 )
 
-func TestMain(m *testing.M) { testingc.M(m, testMain) }
-
-func testMain(m *testingc.MC) int {
+func TestMain(m *testing.M) {
 	ctx := context.Background()
 	err := os.Setenv(beacon.LogLevelEnv, "0")
 	if err != nil {
-		m.Fatal(err)
+		log.Panic(err)
 	}
 
-	connect = database.InitDatabase(m, ctx)
+	var term func()
+	connect, term, err = database.InitDatabase(ctx)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer term()
 	uniqueTenantName, err = unique.String(ctx, 10,
 		unique.WithBeacon("tenant_name"),
 	)
 	if err != nil {
-		m.Fatal(err)
+		log.Panic(err)
 	}
 	uniqueEmployeeUserName, err = unique.String(ctx, 10,
 		unique.WithBeacon("employee_username"),
 	)
 	if err != nil {
-		m.Fatal(err)
+		log.Panic(err)
 	}
 
-	return m.Run()
+	m.Run()
 }
